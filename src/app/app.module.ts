ac740ffeecf31daceabe8a588462f988fb2735eb
kourichiahmed2 <kouraichiahmed@gmailcom>
2023-04-10 00:40:15 +0200

    v1

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+   
+    FooterComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
